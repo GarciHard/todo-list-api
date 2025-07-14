e5009ff56801331e8ee7e151151ddd6e83119cd9
GarciHard <alekzgh@gmail.com>
2025-07-13 23:25:42 -0600

    Introduced ConfigurationProperties to handle values from properties and code enhancements to RabbitMQ producer.

--- src/main/java/com/garcihard/todolist/TodoListApplication.java
+++ src/main/java/com/garcihard/todolist/TodoListApplication.java
@@ -2,10 +2,12 @@ package com.garcihard.todolist;
 
 import org.springframework.boot.SpringApplication;
 import org.springframework.boot.autoconfigure.SpringBootApplication;
+import org.springframework.boot.context.properties.ConfigurationPropertiesScan;
 import org.springframework.cloud.client.discovery.EnableDiscoveryClient;
 import org.springframework.scheduling.annotation.EnableAsync;
 import org.springframework.scheduling.annotation.EnableScheduling;
 
+@ConfigurationPropertiesScan
 @EnableDiscoveryClient
 @EnableAsync
 @EnableScheduling
